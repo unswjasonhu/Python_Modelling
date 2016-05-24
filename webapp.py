b0d67aae6daa7268cba6e36f221d7c386d995147
Hari Bhrugubanda <hbhrugubanda@hotmail.com>
2016-05-24 12:57:07 +0000

    added bulk of index.html, haven't fixed up the get data service yet

--- webapp.py
+++ webapp.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask
 from flask import render_template, request, jsonify
 
@@ -17,18 +19,26 @@ def index():
 
 @app.route('/get_estimates_data', methods = ['GET'])
 def get_estimates_data():
-    #input_datetime = request.form['date_time']
-    #input_date = request.form['date']
-    #lat = float(request.form.getvalue('lat'))
-    #lon = float(request.form.getvalue('lon'))
+    #input_datetime = request.form.get('date_time')
+    #input_date = request.form.get('date')
+    lat = float(request.args.get('lat'))
+    lon = float(request.args.get('lon'))
+    #lat = -33.92313
+    #lon = 150.98812
 
     #print input_datetime
     input_datetime = "2015-09-03 10:00:00"
+    
+    try:
+        time.strptime(input_datetime, "%Y-%m-%d %H:%M:%S")
+    except:
+        pass
 
-    #if debug:
     input_date = "2015-08-05"
-    lat = -33.92313
-    lon = 150.98812
+    try:
+        time.strptime(input_date, "%Y-%m-%d")
+    except:
+        return jsonify({'error': 'Invalid datetime given'})
 
     body = get_estimates_data_service(input_datetime, input_date, lat, lon)
     return jsonify(body)
