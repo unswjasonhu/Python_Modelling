efb405862fbe1dd0a0db28ac0444019bb242aef9
Hari Bhrugubanda <hbhrugubanda@hotmail.com>
2016-07-26 11:18:44 +0000

    hari - updated js and index.html file

--- static/js/custom.js
+++ static/js/custom.js
@@ -3,87 +3,87 @@ var map, heatmap, marker, image;
 
 var myLatLng = {lat: -33.92313, lng: 150.98812};
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 11,
-    scaleControl:true,
-  });
-
-  // Overlay parameters
-  var bounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(62.281819, -150.287132),
-    new google.maps.LatLng(62.400471, -150.005608));
-
-  var srcImage = getPlotUrl();
-  overlay = new EstimatesOverlay(bounds, srcImage, map);
-
-
-  //home marker params
-  home_image = '/images/markers/home.png';
-  marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    icon: home_image,
-    title: 'UNSW is my home'
-  });
-};
-
-
-/**
- * onAdd is called when the map's panes are ready and the overlay has been
- * added to the map.
- */
-EstimatesOverlay.prototype.onAdd = function() {
-
-  var div = document.createElement('div');
-  div.style.borderStyle = 'none';
-  div.style.borderWidth = '0px';
-  div.style.position = 'absolute';
-
-  // Create the img element and attach it to the div.
-  var img = document.createElement('img');
-  img.src = this.image_;
-  img.style.width = '100%';
-  img.style.height = '100%';
-  img.style.position = 'absolute';
-  div.appendChild(img);
-
-  this.div_ = div;
-
-  // Add the element to the "overlayLayer" pane.
-  var panes = this.getPanes();
-  panes.overlayLayer.appendChild(div);
-};
-
-EstimatesOverlay.prototype.draw = function() {
-
-  // We use the south-west and north-east
-  // coordinates of the overlay to peg it to the correct position and size.
-  // To do this, we need to retrieve the projection from the overlay.
-  var overlayProjection = this.getProjection();
-
-  // Retrieve the south-west and north-east coordinates of this overlay
-  // in LatLngs and convert them to pixel coordinates.
-  // We'll use these coordinates to resize the div.
-  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
-  // Resize the image's div to fit the indicated dimensions.
-  var div = this.div_;
-  div.style.left = sw.x + 'px';
-  div.style.top = ne.y + 'px';
-  div.style.width = (ne.x - sw.x) + 'px';
-  div.style.height = (sw.y - ne.y) + 'px';
-};
-
-// The onRemove() method will be called automatically from the API if
-// we ever set the overlay's map property to 'null'.
-EstimatesOverlay.prototype.onRemove = function() {
-  this.div_.parentNode.removeChild(this.div_);
-  this.div_ = null;
-};
-
+//function initMap() {
+//  map = new google.maps.Map(document.getElementById('map'), {
+//    center: myLatLng,
+//    zoom: 11,
+//    scaleControl:true,
+//  });
+//
+//  // Overlay parameters
+//  var bounds = new google.maps.LatLngBounds(
+//    new google.maps.LatLng(62.281819, -150.287132),
+//    new google.maps.LatLng(62.400471, -150.005608));
+//
+//  var srcImage = getPlotUrl();
+//  overlay = new EstimatesOverlay(bounds, srcImage, map);
+//
+//
+//  //home marker params
+//  home_image = '/images/markers/home.png';
+//  marker = new google.maps.Marker({
+//    position: myLatLng,
+//    map: map,
+//    icon: home_image,
+//    title: 'UNSW is my home'
+//  });
+//};
+//
+//
+///**
+// * onAdd is called when the map's panes are ready and the overlay has been
+// * added to the map.
+// */
+//EstimatesOverlay.prototype.onAdd = function() {
+//
+//  var div = document.createElement('div');
+//  div.style.borderStyle = 'none';
+//  div.style.borderWidth = '0px';
+//  div.style.position = 'absolute';
+//
+//  // Create the img element and attach it to the div.
+//  var img = document.createElement('img');
+//  img.src = this.image_;
+//  img.style.width = '100%';
+//  img.style.height = '100%';
+//  img.style.position = 'absolute';
+//  div.appendChild(img);
+//
+//  this.div_ = div;
+//
+//  // Add the element to the "overlayLayer" pane.
+//  var panes = this.getPanes();
+//  panes.overlayLayer.appendChild(div);
+//};
+//
+//EstimatesOverlay.prototype.draw = function() {
+//
+//  // We use the south-west and north-east
+//  // coordinates of the overlay to peg it to the correct position and size.
+//  // To do this, we need to retrieve the projection from the overlay.
+//  var overlayProjection = this.getProjection();
+//
+//  // Retrieve the south-west and north-east coordinates of this overlay
+//  // in LatLngs and convert them to pixel coordinates.
+//  // We'll use these coordinates to resize the div.
+//  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+//  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+//
+//  // Resize the image's div to fit the indicated dimensions.
+//  var div = this.div_;
+//  div.style.left = sw.x + 'px';
+//  div.style.top = ne.y + 'px';
+//  div.style.width = (ne.x - sw.x) + 'px';
+//  div.style.height = (sw.y - ne.y) + 'px';
+//};
+//
+//// The onRemove() method will be called automatically from the API if
+//// we ever set the overlay's map property to 'null'.
+//EstimatesOverlay.prototype.onRemove = function() {
+//  this.div_.parentNode.removeChild(this.div_);
+//  this.div_ = null;
+//};
+//
 
 function getPlotUrl() {
     var datetime = date.value + ' ' + time.value;
